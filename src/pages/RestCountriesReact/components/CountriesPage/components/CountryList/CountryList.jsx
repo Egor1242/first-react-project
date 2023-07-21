a0e5c73f6cd439cc41d2304924813fcdefed466a
DeskorED <rfpfywtdf29@gmail.com>
2023-07-21 15:20:19 +0200

    refactor: modify CountryList

--- src/pages/RestCountriesReact/components/CountriesPage/components/CountryList/CountryList.jsx
+++ src/pages/RestCountriesReact/components/CountriesPage/components/CountryList/CountryList.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
-import {CountryListContext} from "../../../../RestCountriesReact"
+import { CountryListContext } from "../../../../RestCountriesReact"
 
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import { Country } from "./components/Country/Country"
 
 import "./style.sass"
@@ -9,9 +11,27 @@ export const CountryList = () => {
 
     const countryList = useContext(CountryListContext);
 
-    return (
+    return (<>
+        <div className="search">
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={
+                    countryList.map(
+                        country => {
+                            return {
+                                label: country?.data?.name?.official
+                            }
+                        }
+                    )
+                }
+                sx={{ width: "80%", margin: "0 auto", bgcolor: "white", border:"none", borderRadius: "15px" }}
+                renderInput={(params) => <TextField {...params} label="Countries" />}
+            />
+        </div>
         <div className="country-list">
             {countryList?.map((country, index) => <Country key={index} country={country} />)}
         </div>
+    </>
     )
 }
