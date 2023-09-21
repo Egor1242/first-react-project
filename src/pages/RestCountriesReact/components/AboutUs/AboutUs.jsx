f424921510f5c49453cad61e940a2d51e47b5c03
Egor1242 <egorwinner12@gamil.com>
2023-09-21 15:08:22 +0300

    refactor: modify: AboutUs.jsx

--- src/pages/RestCountriesReact/components/AboutUs/AboutUs.jsx
+++ src/pages/RestCountriesReact/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,27 @@
+import "./style.sass"
+
 export const AboutUs = () => {
 
-    return(
+    return (
         <div className="about-us_page">
-                This page about our project
+            <div className="contacts">
+                <div>
+                    Mail: <a href='mailto:mail@egorwinner12@gmail.com'>egorwinner12@gmail.com</a>
+                </div>
+                <div>
+                    Git: <a href='https://github.com/Egor1242'>github</a>
+                </div>
+            </div>
+            <div>
+                <div className="sources">
+                    Sources:
+                    <div><a href='https://restcountries.com'>rest countries</a></div>
+                    <div className="icon-micons"><a href='https://mui.com/material-ui/material-icons/?query=delete'>material-icons</a></div>
+                    <div className="icon-react"><a href='https://ru.legacy.reactjs.org/'>React</a></div>
+                    <div className="icon-js"><a href='https://www.javascript.com'>JavaScript</a></div>
+                    <div className="icon-html"><a href='https://html.com'>HTML</a></div>
+                </div>
+            </div>
         </div>
     )
 }
